b51f6d224563f68f88494560067d414f9d0bc03d
Piotr Żabówka <wooboox@gmail.com>
2025-04-06 16:33:08 +0200

    feat: integrate PostHog for analytics tracking
    - Added PostHog JavaScript and Node.js SDKs to dependencies.
    - Initialized PostHog in the custom App component to track page views.
    - Wrapped the application in PostHogProvider for context.
    - Created a PostHog client for server-side usage.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://eu-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://eu.i.posthog.com/:path*',
+      },
+      {
+        source: '/ingest/decide',
+        destination: 'https://eu.i.posthog.com/decide',
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
+};
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
 import '../styles/main.scss'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import { Router } from 'next/router'
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+      api_host: "/ingest",
+      ui_host: "https://eu.posthog.com",
+      loaded: (posthogInstance) => {
+        if (process.env.NODE_ENV === "development") posthogInstance.debug()
+      },
+    })
+
+    const handleRouteChange = () => posthog?.capture("$pageview")
+    Router.events.on("routeChangeComplete", handleRouteChange)
+
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [])
+
+  return (
+    <PostHogProvider client={posthog}>
+      <Component {...pageProps} />
+    </PostHogProvider>
+  )
 }
 
 export default MyApp
